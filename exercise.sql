26b51b840ef8d8d33aeeb13d2641953e01fef5b3
TelRich <okorogoodrich@gmail.com>
2023-01-10 20:14:34 +0100

    added answer 2

--- exercise.sql
+++ exercise.sql
@@ -1,4 +1,9 @@
 -- Distribution of stroke in the dataset
 SELECT      gender, stroke, COUNT(*) count
 FROM        brainStroke
-GROUP BY    gender, stroke,
+GROUP BY    gender, stroke;
+
+SELECT      stroke, CAST(COUNT(*) AS FLOAT)/total.count * 100 AS Percent
+FROM        brainStroke, (SELECT COUNT(*) count FROM brainStroke WHERE gender = 'Male') total
+WHERE       gender = 'Male'
+GROUP BY    stroke;
